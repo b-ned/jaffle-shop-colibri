40e341b77c74fefc5b5030f40803d274ee880965
Bas Nederkoorn <awsnederkoorn@gmail.com>
2025-10-19 08:43:40 +0200

    trino-example

--- models/marts/downstream_report_with_a_very_very_very_long_name.sql
+++ models/marts/downstream_report_with_a_very_very_very_long_name.sql
@@ -1,3 +1,3 @@
 select *,
-         'downstream_report_with_a_very_very_very_long_name' as this_is_a_very_very_very_long_column_name_with_a_very_very_very_long_description
+         'downstream_report_with_a_very_very_very_long_name' as this_is_a_very_very_very_long_column
 from {{ ref('report')}}
---
+++ scratch.sh
@@ -0,0 +1,4 @@
+docker run -d \
+  --name trino_dev \
+  -p 8080:8080 \
+  trinodb/trino:latest
